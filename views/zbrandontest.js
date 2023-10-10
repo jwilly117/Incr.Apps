771cf0c0b29f5a0a005732d0f17d868847aa8163
Jake <jakewilliams117@gmail.com>
2023-10-10 14:28:34 -0400

    Adding loop test to RTS DATA

--- views/zbrandontest.js
+++ views/zbrandontest.js
@@ -1234,8 +1234,8 @@ function getRTS(){
       console.log("Here is the real test of diving down into the object");
 
       console.log(response.data);
-      // console.log(response.data.report_query_iirts_b0e1556ea666496ba627a81cf58a623d.items[1]);
-      console.log(response.data.report_query_iirts_b0e1556ea666496ba627a81cf58a623d.items[2])
+      console.log(response.data.report_query_iirts_b0e1556ea666496ba627a81cf58a623d.items);
+      // console.log(response.data.report_query_iirts_b0e1556ea666496ba627a81cf58a623d.items[2])
       });
 
     }
